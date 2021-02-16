ed23b4d71e3b94c66f944185b535fcbe1126b0e5
czagrzebski <48450104+czagrzebski@users.noreply.github.com>
2021-02-16 14:00:51 -0600

    Schematic for Wireless Station

--- testing/NRFTest/NRFTransmitter/NRFTransmitter.ino
+++ testing/NRFTest/NRFTransmitter/NRFTransmitter.ino
@@ -6,11 +6,11 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-RF24 radio(7, 8); // CE, CSN
-const byte address[6] = "00001";
+RF24 radio(7, 8); // CE (Pin 7), CSN (Pin 8)
+const byte address[6] = "00001"; //radio address
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //9600 baudrate for serial connection
   Serial.println("WeatherStation v1.0");
   radio.begin();
   radio.openWritingPipe(address);
